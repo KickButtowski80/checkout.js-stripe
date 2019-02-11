c94d07a205365f49e878a7b2f5dcf6443484b8e0
kickbuttowski59 <pazpaz22@yahoo.com>
2019-02-11 23:54:32 +0000

    omitted unwanted migration file

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
     belongs_to :user, :optional => true
     # has_many :purchases
+    belongs_to :order
+    belongs_to :purchase
 end
--- db/migrate/20190208190300_add_purchase_id_to_order.rb
+++
@@ -1,5 +0,0 @@
-class AddPurchaseIdToOrder < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :orders, :purchase, foreign_key: true
-  end
-end
