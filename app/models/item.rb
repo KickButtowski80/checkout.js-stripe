8d5ff03359833f70db9bf96bc69ee59530de42e9
kickbuttowski59 <pazpaz22@yahoo.com>
2019-02-12 00:04:31 +0000

    fixing order and purchase exist issue

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
     belongs_to :user, :optional => true
     # has_many :purchases
-    belongs_to :order
-    belongs_to :purchase
+    belongs_to :order, :optional => true
+    belongs_to :purchase, :optional => true
 end
