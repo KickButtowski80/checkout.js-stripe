bcb9c0a0588aa89d5523cc438f12594bcf854647
kickbuttowski59 <pazpaz22@yahoo.com>
2019-02-15 06:04:58 +0000

    user has many purchases so needs dpendent destroy for purchases when the user is omitted

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :purchases
+  has_many :purchases , dependent: :destroy
 end
